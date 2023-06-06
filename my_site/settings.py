185ed4402e6442a6cda408ff2d2bb71f715e2db9
Luisa Faria <luisa.f@aluno.ifsp.edu.br>
2023-06-06 11:33:55 -0300

    MIGRATIONS

--- my_site/settings.py
+++ my_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pollsdb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-return HttpResponse("Olá Mundo! Esta é a página inicial do aplicativo")
+    return HttpResponse("Olá Mundo! Esta é a página inicial do aplicativo")
 
 def produtos(request):
-return HttpResponse("Esta é a página dos produtos.")
+    return HttpResponse("Esta é a página dos produtos.")
