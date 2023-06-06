3752ff7ca36f3d65635a4491216c7da482f1d9e1
Luisa Faria <luisa.f@aluno.ifsp.edu.br>
2023-06-06 11:13:45 -0300

    criação dos polls

---
+++ polls/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+return HttpResponse("Olá Mundo! Esta é a página inicial do aplicativo")
+
+def produtos(request):
+return HttpResponse("Esta é a página dos produtos.")
