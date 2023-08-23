28a5775f4a55ceb812f25d37518d57bc35c9022f
MiihNardoni <michellesilva2407@gmail.com>
2023-08-23 09:29:27 -0300

    23.08

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    path('cadastrar', 
+        views.QuestionCreateView.as_view(),
+        name ="question-create")
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,27 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-
-
-def detail(request, question_id):
-    return HttpResponse(f"Essa é a pergunta número {question_id}.")
-
-def results(request, question_id):
-    return HttpResponse(f"Resultados da pergunta de número {question_id}.")
-
-def vote(request, question_id):
-    return HttpResponse(f"Você está votando na pergunta {question_id}.")
+from .models import Question 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+    return render(request, "polls/detail.html", context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,"polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    response = f'Resultados da pergunta de número {question_id}'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Você está votando em uma enquete %s." % question_id)
+
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+class QuestionCreateView(CreateView):
+    model = Question
+    success_url = reverse_lazy('index')
