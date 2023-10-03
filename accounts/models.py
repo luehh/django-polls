07be8703b7df28f404caf82caa8d4ad9eff7b641
MiihNardoni <michellesilva2407@gmail.com>
2023-10-02 21:27:24 -0300

    aa

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser): # herda o model User base padrão do Django
+    data_nascimento = models.fields.DateField(
+        "Data de Nascimento", null=True, blank=True)
+cpf = models.CharField("CPF", max_length=11, null=True, blank=True)
+
 
 # Create your models here.
